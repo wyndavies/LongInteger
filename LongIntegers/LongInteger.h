e1709fc14bee53edcd8b1595aa89a2ae74032c35
wydavies <wyndhamdavies@virginmedia.com>
2017-03-16 14:16:07 +0000

    Added a template declaration that lets the code build under C++11

--- LongIntegers/LongInteger.h
+++ LongIntegers/LongInteger.h
@@ -72,6 +72,13 @@ using std::stringstream;
 using std::string;
 #endif
 
+#if __cplusplus == 201103L
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args) {
+	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+#endif
+
 using std::vector;
 using std::unique_ptr;
 using std::make_unique;
